960cb1cb1c2f0c8269454918e9cc1da18cdc977c
potyama <kamikaze117117@gmail.com>
2021-09-01 11:22:56 +0900

    足りないもの追加

--- js/calc.js
+++ js/calc.js
@@ -1,14 +1,17 @@
-    function calc(elem, jun, high, gr, ex_elem, ex_juni, ex_high, ex_salary, gr_per_hour, h_per_hour, sj_per_hour, ad_salary, ad_per_hour) {
+    function calc(elem, jun, high, gr,jimu, kensyu, ex_gr, ex_elem, ex_juni, ex_high, ex_salary, gr_per_hour, h_per_hour, sj_per_hour, ad_salary, ad_per_hour) {
         //通常
         const ele_val = document.getElementById('first').value = Math.round(elem * (sj_per_hour * 0.75));
         const jun_val = document.getElementById('second').value = Math.round(jun * (sj_per_hour * 1.5));
         const high_val = document.getElementById('third').value = Math.round(high * (h_per_hour * 1.5));
         const gr_val = document.getElementById('fourth').value = Math.round(gr * gr_per_hour);
-        const ad_normal_val = document.getElementById('ad_normal').value = Math.round((Math.ceil(((ad_salary * 5)/60)*100)/100 )* 930);
+        const jimu_val = ocument.getElementById('fifth').value = Math.round((Math.ceil(((jimu)/60)*100)/100 )* ad_per_hour);
+        const kensyu_val = ocument.getElementById('sixth').value = Math.roundMath.round((Math.ceil(((kensyu * 5)/60)*100)/100 )* ad_per_hour);
+        const ad_normal_val = document.getElementById('ad_normal').value = Math.round((Math.ceil(((ad_salary * 5)/60)*100)/100 )* ad_per_hour);
         //講習
-        const ex_ele_val = document.getElementById('fifth').value = Math.round(ex_elem * (sj_per_hour * 0.75));
-        const ex_jun_val = document.getElementById('sixth').value = Math.round(ex_juni * (sj_per_hour * 1.5));
-        const ex_high_val = document.getElementById('seventh').value = Math.round(ex_high * (h_per_hour * 1.5));
+        const ex_ele_val = document.getElementById('seventh').value = Math.round(ex_elem * (sj_per_hour * 0.75));
+        const ex_jun_val = document.getElementById('eighth').value = Math.round(ex_juni * (sj_per_hour * 1.5));
+        const ex_high_val = document.getElementById('ninth').value = Math.round(ex_high * (h_per_hour * 1.5));
+        const jimu_val = ocument.getElementById('tenth').value = Math.round(ex_gr * gr_per_hour);
         const ad_ex_val = document.getElementById('ad_ex').value = Math.round((Math.ceil(((ex_salary * 5)/60)*100)/100 )* 930);
         //それぞれの合計
         const no_total_val = document.getElementById('no_total').value = Math.round(ele_val + jun_val + high_val + gr_val + ad_normal_val);
